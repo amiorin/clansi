c95d2feedc071b41a7a29845a949c17f614b8437
Larry Gouger <larry.gouger@coxautoinc.com>
2019-05-01 14:53:52 -0400

    added IntelliJ IDEA config files.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 
 (defproject clansi "1.3.1-LPG-SNAPSHOT"
   :description "ANSI style and color codes for Clojure."
-  :dependencies [[org.clojure/clojure "1.9.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :main clansi.core)
